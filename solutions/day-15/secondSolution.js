6ffb5695eb433ec22ed5d078abffdb0f108aedeb
ilker <ilkerkaran@gmail.com>
2023-12-15 13:37:55 +0000

    day15 part 1 complete

--- solutions/day-15/secondSolution.js
+++ solutions/day-15/secondSolution.js
@@ -2,9 +2,10 @@ import { decodeLabel } from './utils.js'
 
 export default (arr) => {
   const boxes = []
+  boxes.fill([], 0, 255)
   const steps = arr.flatMap(x => x.split(','))
 
-  console.log(decodeLabel('qp'))
+  console.log(decodeLabel('qp'), boxes.length)
 
   // to be continued...
 }
